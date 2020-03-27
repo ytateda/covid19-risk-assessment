f20b288af7a8e3fbc1478fb50db271555f7ee375
rushankgoyal <60558881+rushankgoyal@users.noreply.github.com>
2020-03-26 21:45:56 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,13 +1,19 @@
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup
+def return_active_cases(country):
+  import pandas as pd
+  import requests
+  from bs4 import BeautifulSoup
 
-req = requests.get("https://www.worldometers.info/coronavirus/")
-soup = BeautifulSoup(req.content)
+  req = requests.get("https://www.worldometers.info/coronavirus/")
+  soup = BeautifulSoup(req.content)
 
-data = list(map(lambda x: list(map(lambda y: y.text, x.select("td, th"))),soup.select("table#main_table_countries_today tr")))
+  data = list(map(lambda x: list(map(lambda y: y.text, x.select("td, th"))),soup.select("table#main_table_countries_today tr")))
 
-df = pd.DataFrame(data)
-df.columns = df.iloc[0]
-df = df.drop(0, axis=0)
-df.to_csv("covid19.csv")
+  df = pd.DataFrame(data)
+  df.columns = df.iloc[0]
+  df = df.drop(0, axis=0)
+  df = df.fillna(0)
+
+  i = df.loc[df['Country']==country_name].index[0]
+  active_cases = df['ActiveCases'][i]
+
+  return active_cases
