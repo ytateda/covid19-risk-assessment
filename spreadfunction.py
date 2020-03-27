d41474dd3a73a533bd8a67e24a855642d16ec784
ytateda <50935246+ytateda@users.noreply.github.com>
2020-03-27 11:52:37 -0700

    new variable, Movement
    how many grid movement per hour.

--- spreadfunction.py
+++ spreadfunction.py
@@ -4,7 +4,7 @@ import random
 from matplotlib import pyplot as plt
 from IPython.display import clear_output
 
-def viral_spread(population,time,initial):
+def viral_spread(population,time,initial,movement):
   grid_space=int(math.sqrt(population))
   grid=np.zeros((grid_space,grid_space))
   for i in range(0,initial):
@@ -31,6 +31,14 @@ def viral_spread(population,time,initial):
           x_step=i+np.random.randint(-1,2)
           y_step=j+np.random.randint(-1,2)
           grid[x_step,y_step]=1
+          
+          x_switch=0
+          y_switch=0
+          x_switch=i+np.random.randint(-movement,movement+1)
+          y_switch=j+np.random.randint(-movement,movement+1)
+          if x_switch < grid_space-1 & x_switch>0 & y_switch<grid_space-1 & y_switch> 0:
+            grid[x_switch,y_switch]=1
+            grid[i,j]=0
 
     infectioncount.append(np.count_nonzero(grid))
     plt.figure()
